7bd88fd71cbfd140c30ebc8adc46785d104af637
Aleksandr-hub-cyber <141833331+Aleksandr-hub-cyber@users.noreply.github.com>
2024-11-28 20:09:32 +0800

    first commit

---
+++ handlers.py
@@ -0,0 +1,52 @@
+from aiogram import types
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram import F
+from quiz_data import quiz_data
+from database import get_quiz_index, update_quiz_index
+from aiogram import Dispatcher, types
+
+dp = Dispatcher()
+
+def generate_options_keyboard(answer_options, right_answer):
+    builder = InlineKeyboardBuilder()
+
+    for option in answer_options:
+        builder.add(types.InlineKeyboardButton(
+            text=option,
+            callback_data="right_answer" if option == right_answer else "wrong_answer")
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.add(types.KeyboardButton(text="Начать игру"))
+    await message.answer("Добро пожаловать в квиз!", reply_markup=builder.as_markup(resize_keyboard=True))
+
+
+async def get_question(message, user_id):
+
+    # Получение текущего вопроса из словаря состояний пользователя
+    current_question_index = await get_quiz_index(user_id)
+    correct_index = quiz_data[current_question_index]['correct_option']
+    opts = quiz_data[current_question_index]['options']
+    kb = generate_options_keyboard(opts, opts[correct_index])
+    await message.answer(f"{quiz_data[current_question_index]['question']}", reply_markup=kb)
+
+
+async def new_quiz(message):
+    user_id = message.from_user.id
+    current_question_index = 0
+    await update_quiz_index(user_id, current_question_index)
+    await get_question(message, user_id)
+
+# Хэндлер на команду /quiz
+@dp.message(F.text=="Начать игру")
+@dp.message(Command("quiz"))
+async def cmd_quiz(message: types.Message):
+
+    await message.answer(f"Давайте начнем квиз!")
+    await new_quiz(message)
---
+++ telegrammbot.py
@@ -0,0 +1,70 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram import F
+from quiz_data import API_TOKEN, quiz_data
+from database import create_table, get_quiz_index, update_quiz_index
+from handlers import get_question, dp
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+
+@dp.callback_query(F.data == "right_answer")
+async def right_answer(callback: types.CallbackQuery):
+
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    await callback.message.answer("Верно!")
+    current_question_index = await get_quiz_index(callback.from_user.id)
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await update_quiz_index(callback.from_user.id, current_question_index)
+
+
+    if current_question_index < len(quiz_data):
+        await get_question(callback.message, callback.from_user.id)
+    else:
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
+
+
+@dp.callback_query(F.data == "wrong_answer")
+async def wrong_answer(callback: types.CallbackQuery):
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    # Получение текущего вопроса из словаря состояний пользователя
+    current_question_index = await get_quiz_index(callback.from_user.id)
+    correct_option = quiz_data[current_question_index]['correct_option']
+
+    await callback.message.answer(f"Неправильно. Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
+
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await update_quiz_index(callback.from_user.id, current_question_index)
+
+
+    if current_question_index < len(quiz_data):
+        await get_question(callback.message, callback.from_user.id)
+    else:
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+
+    # Запускаем создание таблицы базы данных
+    await create_table()
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
